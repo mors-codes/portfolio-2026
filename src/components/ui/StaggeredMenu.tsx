2ee02886ccfc9d4506834fca9b640fea6d5b6fe1
mors-codes <morsmatias15@gmail.com>
2026-09-17 17:07:00 +0800

    Fix staggered menu mobile viewport height and scroll lock

--- src/components/ui/StaggeredMenu.tsx
+++ src/components/ui/StaggeredMenu.tsx
@@ -329,6 +329,24 @@ const StaggeredMenu = forwardRef<StaggeredMenuHandle, StaggeredMenuProps>(
       };
     }, [closeOnClickAway, open, closeMenu]);
 
+    useLayoutEffect(() => {
+      if (open) {
+        const scrollY = window.scrollY;
+        document.body.style.position = "fixed";
+        document.body.style.top = `-${scrollY}px`;
+        document.body.style.width = "100%";
+        document.body.style.overflow = "hidden";
+
+        return () => {
+          document.body.style.position = "";
+          document.body.style.top = "";
+          document.body.style.width = "";
+          document.body.style.overflow = "";
+          window.scrollTo(0, scrollY);
+        };
+      }
+    }, [open]);
+
     return (
       <div
         className={
